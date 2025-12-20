21b696b4003fa0a390633e8439ddb359ae3623b2
sflabbe <sflabbe@gmail.com>
2025-12-20 10:58:27 +0100

    cleanup: remove legacy code and ignore build artifacts

--- legacy/dinamica_computacional/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-__all__ = ["core", "elements", "materials", "integrators", "io", "utils"]
--- legacy/dinamica_computacional/materials/linear_elastic.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from dataclasses import dataclass
-
-
-@dataclass
-class LinearElastic:
-    E: float
-    nu: float = 0.3
